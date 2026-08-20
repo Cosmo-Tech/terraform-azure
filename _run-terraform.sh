9bd974be82b102431bee835e6a291a395b4ede0e
ggtrd <103002419+ggtrd@users.noreply.github.com>
2026-08-20 16:46:50 +0200

    enable comment detection in get_var_value & add some QoL

--- _run-terraform.sh
+++ _run-terraform.sh
@@ -5,11 +5,17 @@
 # - ./script.sh
 
 
+# Colors
+NO_FORMAT="\033[0m"
+FG_COLOR_INFO="\033[38;5;141m"
+FG_COLOR_WARN="\033[38;5;203m"
+
+
 # Stop script if missing dependency
 required_commands="terraform az"
 for command in $required_commands; do
     if [ -z "$(command -v $command)" ]; then
-        echo "error: required command not found: \e[91m$command\e[97m"
+        echo "error: required command not found: $FG_COLOR_WARN$command$NO_FORMAT"
         exit 1
     fi
 done
@@ -21,11 +27,12 @@ get_var_value() {
     local file=$1
     local variable=$2
 
-    cat $file | grep '=' | grep -w $variable | sed 's|.*"\(.*\)".*|\1|' | head -n 1
+    cat $file | grep '=' | grep -w $variable | sed '/.*#.*/d' | sed 's|.*=.*"\(.*\)".*|\1|' | head -n 1
 }
 cluster_name="$(get_var_value terraform-cluster/terraform.tfvars cluster_name)"
 cluster_stage="$(get_var_value terraform-cluster/terraform.tfvars cluster_stage)"
 cluster_region="$(get_var_value terraform-cluster/terraform.tfvars cluster_region)"
+cluster_full_name="aks-$cluster_stage-$cluster_name"
 
 azure_subscription_id="$(get_var_value terraform-cluster/terraform.tfvars azure_subscription_id)"
 azure_entra_tenant_id="$(get_var_value terraform-cluster/terraform.tfvars azure_entra_tenant_id)"
@@ -63,27 +70,33 @@ fi
 rm -rf terraform-cluster/.terraform*
 rm -rf terraform-cluster/terraform.tfstate*
 
-echo ""
-echo "\e[92mDeploying terraform-cluster...\e[0m"
-echo ""
+echo "$FG_COLOR_INFO"
+echo "Deploying terraform-cluster..."
+echo "$NO_FORMAT"
 
 terraform -chdir=terraform-cluster init -upgrade -reconfigure \
     -backend-config="storage_account_name=$state_storage_name" \
     -backend-config="container_name=$state_storage_name" \
     -backend-config="resource_group_name=$state_storage_name" \
-    -backend-config="key=tfstate-cluster-aks-$cluster_stage-$cluster_name"
+    -backend-config="key=tfstate-cluster-$cluster_full_name"
 terraform -chdir=terraform-cluster plan -out .terraform.plan
+
+echo "$NO_FORMAT"
+echo "target is $FG_COLOR_INFO$cluster_full_name"
+echo "$NO_FORMAT"
 
 option_apply='--apply'
 if [ "$(echo $1)" = "$option_apply" ]; then
     terraform -chdir=terraform-cluster apply .terraform.plan
 
     echo "add the cluster to your kubeconfig:"
-    echo "az aks get-credentials --resource-group aks-$cluster_stage-$cluster_name --name aks-$cluster_stage-$cluster_name --overwrite-existing"
+    echo "$FG_COLOR_INFO"
+    echo "az aks get-credentials --resource-group $cluster_full_name --name $cluster_full_name --overwrite-existing"
+    echo "$NO_FORMAT"
 else
-  echo ''
-  echo "\e[97mTerraform plan can be applied with:"
-  echo "  $0 $option_apply"
+    echo "$NO_FORMAT"
+    echo 'Terraform plan can be applied with:'
+    echo "$FG_COLOR_INFO  $0 $option_apply$NO_FORMAT"
 fi
 
 
